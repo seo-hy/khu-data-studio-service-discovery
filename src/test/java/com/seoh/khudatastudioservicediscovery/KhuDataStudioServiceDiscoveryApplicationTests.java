106010c70682f504221ea6d0569f8bd2fb2d0e2f
seoh02h <seok02h@khu.ac.kr>
2022-10-25 23:53:21 +0900

    Add service discovery

---
+++ src/test/java/com/seoh/khudatastudioservicediscovery/KhuDataStudioServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoh.khudatastudioservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuDataStudioServiceDiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
